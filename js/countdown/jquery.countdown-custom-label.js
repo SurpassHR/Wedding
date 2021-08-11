e7abed2f656d177708942900cf2f0a212eefa452
SurpassHR <hu.rui0530@gmail.com>
2021-08-11 16:21:12 +0800

    first publish

--- js/countdown/jquery.countdown-custom-label.js
+++ js/countdown/jquery.countdown-custom-label.js
@@ -8,7 +8,7 @@
         digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
         timeSeparator: ':',
         isRTL: false,
-        format: 'YdHM'
+        format: 'YdHMs'
     };
     $.countdown.setDefaults($.countdown.regionalOptions['bg']);
 })(jQuery);
